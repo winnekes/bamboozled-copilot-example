4812be4cd3873c5972995d514b59c2f26697485a
winnekes <simona.winnekes@googlemail.com>
2021-07-15 12:03:06 +0200

    Pair program: create initial heading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-import { Text } from "@chakra-ui/react";
+import { Center, Heading, VStack } from "@chakra-ui/react";
 
 export default function Home() {
-  return <Text>Welcome!</Text>;
+  /* Render a Center with h of "100vh" and a VStack.
+  The VStack is a stack of two elements:
+  - a Heading as="h1" and size="3xl" with Text "Bamboozled"
+  - another Heading as="h2" and size="2xl" with Text "made with love by Simona and Github Copilot"
+   */
+  return (
+    <Center h="100vh">
+      <VStack>
+        <Heading as="h1" size="3xl">
+          Bamboozled
+        </Heading>
+        <Heading as="h2" size="2xl">
+          made with love by Simona and Github Copilot
+        </Heading>
+      </VStack>
+    </Center>
+  );
 }
